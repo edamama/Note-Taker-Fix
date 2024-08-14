990956a000bd6015d9a4e9c62db05efff5f336fb
Tyler Payne <169082073+edamama@users.noreply.github.com>
2024-08-14 17:22:23 -0500

    2
    added the server.js code to run the app

---
+++ routes/api_routes.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+//const {v4: uuidv4 } = require("uuid");
+const router = require("express").Router();
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const html = require("./routes/html_routes.js");
+const api_routes = require("./routes/api_routes.js");
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html);
+
+
+app.listen(PORT, ()=> {
+
+    console.log(`We are up at http:localhost:${PORT}`);
+
+});
+
+
